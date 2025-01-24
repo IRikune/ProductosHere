b9c979ff03d6d1b1c483ece7756f321e8a188210
CodeArmer <geanmotas@gmail.com>
2025-01-24 09:24:37 -0400

    Fixed responses to handle like kv

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -19,7 +19,7 @@ interface KvUsersResult {
 }
 type KvUsersResultMaybe = Promise<KvUsersResult>
 
-export async function getAllUsers(
+export async function getManyUsers(
   { prefix = "" }: GetManyUsersOptions,
 ): KvUsersResultMaybe {
   const primaryKey = [...prefix]
@@ -33,7 +33,7 @@ export async function getAllUsers(
   return res
 }
 
-export async function createUser(user: User) {
+export async function createUser(user: User): KvUsersResultMaybe {
   const primaryKey = ["users", user.id]
   const byEmailKey = ["users", "email", user.email]
   const res = await kv.atomic()
@@ -45,18 +45,19 @@ export async function createUser(user: User) {
   return res
 }
 
-export async function getUser(userID: User["id"]) {
+export async function getUser(userID: User["id"]): KvUsersResultMaybe {
   const primaryKey = ["users", userID]
-  const res = await kv.get<User>(primaryKey)
-  if (!res.versionstamp) {
+  const user = await kv.get<User>(primaryKey)
+  if (!user.versionstamp) {
     throw new TypeError("User not registered")
   }
-  return res.value
+  const response = { ok: true, ...user }
+  return response
 }
 
 export async function updateUser(
   { userEmail, userID, newUser }: UpdateUserOptions,
-) {
+): KvUsersResultMaybe {
   const primaryKey = ["user", userID]
   const byEmailKey = ["user", "email", userEmail]
   const res = await kv.atomic()
@@ -66,8 +67,11 @@ export async function updateUser(
   if (!res.ok) {
     throw new TypeError("User with ID or email not in exists")
   }
+  return res
 }
-export async function deleteUser({ userID, userEmail }: DeleteUserOptions) {
+export async function deleteUser(
+  { userID, userEmail }: DeleteUserOptions,
+): KvUsersResultMaybe {
   const primaryKey = ["user", userID]
   const byEmailKey = ["user", "email", userEmail]
   const res = await kv.atomic()
@@ -79,4 +83,5 @@ export async function deleteUser({ userID, userEmail }: DeleteUserOptions) {
   if (!res.ok) {
     throw new TypeError("User with ID or email not in exists")
   }
+  return res
 }
