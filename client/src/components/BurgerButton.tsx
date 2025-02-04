59d9d198a54fc5a01869cf29ef2216b8f0036a23
CodeArmer <geanmotas@gmail.com>
2025-02-04 03:10:14 -0400

    Added wiggle animation and presentation doodle to login and sign up

--- client/src/components/BurgerButton.tsx
+++ client/src/components/BurgerButton.tsx
@@ -14,7 +14,7 @@ export function BurgerButton() {
         >
             <button
                 onClick={() => setIsOpen(!isOpen)}
-                className='w-10 h-10 right-14 top-8 absolute rounded flex justify-center items-center transition duration-300 active:bg-neutral-200 z-20 overflow-hidden starting:opacity-0'>
+                className='cursor-pointer w-10 h-10 right-14 top-8 absolute rounded flex justify-center items-center transition duration-300 active:bg-neutral-200 z-20 overflow-hidden starting:opacity-0'>
                 <div className={`rounded w-[80%] p-px bg-black self-start absolute mt-2 ${isOpen ? 'animation-top-bar-open' : 'animation-top-bar-close'}`}></div>
                 <div className={`rounded w-[80%] p-px bg-black absolute ${isOpen ? 'animation-middle-bar-open' : 'animation-middle-bar-close'}`}></div>
                 <div className={`rounded w-[80%] p-px bg-black self-end absolute mb-2 ${isOpen ? 'animation-bottom-bar-open' : 'animation-bottom-bar-close'}`}>
@@ -31,7 +31,7 @@ function Menu({ isOpen }: { isOpen: boolean }) {
     return (
         <div
             className={`
-                fixed bg-white -left-[100dvw] w-dvw h-dvh -top-1 py-10 -z-50 flex items-center 
+                fixed bg-white -left-[100dvw] w-dvw h-dvh -top-1 py-10 -z-50 flex items-center
                 justify-center flex-col ${isOpen ? 'animation-slide z-10' : 'animation-slide-out z-10'}`}>
             <a href='/feed'>
                 <Button type='outlined' className='text-3xl my-2'>Feed</Button>
