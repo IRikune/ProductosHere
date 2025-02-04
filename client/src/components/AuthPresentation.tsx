a91a9078d21a24752eae5e0d1beaeb8f72805d7c
CodeArmer <geanmotas@gmail.com>
2025-02-04 15:43:47 -0400

    Refactor: auth presentation screen

---
+++ client/src/components/AuthPresentation.tsx
@@ -0,0 +1,16 @@
+
+export function AuthPresentation() {
+    return (
+        <section className="font-drawed text-center presentation">
+            <h1 className="font-drawed text-6xl" />
+            <img
+                src="../../public/ducks.png"
+                alt="ducks"
+                className="w-96 -my-15 animate-pulse-2"
+            />
+            <h2 className="text-4xl">¿Te quieres unir a nosotros?</h2>
+            <h3 className="text-5xl">¡Bienvenido!</h3>
+        </section>
+    )
+}
+
