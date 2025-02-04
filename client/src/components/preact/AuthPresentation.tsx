d9f966994f86c8b46990e9dce64f30d961063b2b
CodeArmer <geanmotas@gmail.com>
2025-02-04 16:39:20 -0400

    Doodle component added

--- client/src/components/preact/AuthPresentation.tsx
+++ client/src/components/preact/AuthPresentation.tsx
@@ -1,17 +1,17 @@
 import { LovingDoodle } from "../doodles/LovingDoodle"
 export function AuthPresentation() {
     return (
-        <section className="font-drawed text-center presentation">
-            <h1 className="font-drawed text-6xl">
+        <section class="font-drawed text-center presentation">
+            <h1 class="font-drawed text-6xl">
                 ProductosHere
             </h1>
             <img
                 src="../../public/ducks.png"
                 alt="ducks"
-                className="w-96 -my-15 animate-pulse-2"
+                class="w-96 -my-15 animate-pulse-2"
             />
-            <h2 className="text-4xl">¿Te quieres unir a nosotros?</h2>
-            <h3 className="text-5xl">¡Bienvenido!</h3>
+            <h2 class="text-4xl">¿Te quieres unir a nosotros?</h2>
+            <h3 class="text-5xl">¡Bienvenido!</h3>
         </section>
     )
 }
