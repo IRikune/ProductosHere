f06dd8ec306dbbecaf4c2e2475750f535740faa2
CodeArmer <geanmotas@gmail.com>
2025-02-08 02:41:46 -0400

    Updated all svgs and removed unused components

--- client/src/components/preact/Input.tsx
+++ client/src/components/preact/Input.tsx
@@ -8,8 +8,11 @@ interface InputProps {
 export function Input({ placeholder, id, type, class: className }: InputProps) {
     return (
         <div class="tracking-widest">
-            <input class={`font-drawed w-full rounded focus:outline-offset-1 outline-neutral-500 p-2 focus:outline-none text-3xl  after:content-none bg-neutral-200 ${className}`}
-                placeholder={placeholder} id={id} type={type} />
+            <input
+                class={`font-drawed w-full rounded focus:outline-offset-1 outline-neutral-500 p-2 focus:outline-none text-3xl  after:content-none bg-neutral-200 ${className}`}
+                placeholder={placeholder}
+                id={id}
+                type={type} />
         </div>
     )
 }
