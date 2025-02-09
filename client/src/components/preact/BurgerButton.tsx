95d0b85224f24f9ddcf9af7d7c521bd96d36a305
CodeArmer <geanmotas@gmail.com>
2025-02-09 02:11:51 -0400

    Updated burger menu

--- client/src/components/preact/BurgerButton.tsx
+++ client/src/components/preact/BurgerButton.tsx
@@ -1,38 +1,36 @@
-import { useState, useEffect } from 'preact/hooks';
+import { type Signal, useSignal, useSignalEffect } from '@preact/signals';
 import { Button } from './Button';
 import '../../assets/animations.css'
 
 export function BurgerButton({ class: className }: { class?: string }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        setIsLoading(false)
-    }, [])
-
+    const isOpen = useSignal(false);
+    const isAnimating = useSignal(true);
     return (
         <div>
             <button
                 type="button"
-                onClick={() => setIsOpen(!isOpen)}
+                onClick={() => { isOpen.value = !isOpen.value }}
                 class='cursor-pointer w-10 h-10 right-14 top-8 absolute rounded flex justify-center items-center transition duration-300 active:bg-neutral-200 z-30 overflow-hidden starting:opacity-0'>
-                <div class={`rounded w-[80%] p-px bg-black self-start absolute mt-2 ${isOpen ? 'animation-top-bar-open' : 'animation-top-bar-close'}`} />
-                <div class={`rounded w-[80%] p-px bg-black absolute ${isOpen ? 'animation-middle-bar-open' : 'animation-middle-bar-close'}`} />
-                <div class={`rounded w-[80%] p-px bg-black self-end absolute mb-2 ${isOpen ? 'animation-bottom-bar-open' : 'animation-bottom-bar-close'} ${className}`}>
+                <div
+                    class={`rounded w-[80%] p-px bg-black self-start absolute mt-2 ${isOpen.value ? 'animation-top-bar-open' : 'animation-top-bar-close'}`} />
+                <div
+                    class={`rounded w-[80%] p-px bg-black absolute ${isOpen.value ? 'animation-middle-bar-open' : 'animation-middle-bar-close'}`} />
+                <div
+                    class={`rounded w-[80%] p-px bg-black self-end absolute mb-2 ${isOpen.value ? 'animation-bottom-bar-open' : 'animation-bottom-bar-close'} ${className}`}>
                 </div>
             </button>
-            <div
-                class="-z-50">
+            <div>
                 <Menu isOpen={isOpen} />
             </div>
         </div>
     );
 }
-function Menu({ isOpen }: { isOpen: boolean }) {
+function Menu({ isOpen }: { isOpen: Signal<boolean> }) {
     return (
         <div
             class={`
-                fixed bg-[#f4f2ed] -left-[100dvw] w-dvw h-dvh -top-1 py-10 z-20 flex items-center
-                justify-center flex-col ${isOpen ? 'animation-slide z-20' : 'animation-slide-out z-10'}`}>
+                fixed bg-[#f4f2ed] left-0 w-dvw h-dvh -top-1 py-10 z-20 flex items-center justify-center flex-col starting:opacity-0 transition-all duration-300 ease-in-out transition-discrete
+                ${isOpen.value ? 'opacity-100 translate-0' : 'opacity-0 hidden'}`}>
             <a href='/feed'>
                 <Button type='outlined' class='text-3xl my-2'>Feed</Button>
             </a>
