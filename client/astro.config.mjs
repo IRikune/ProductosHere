e28f19210f77111dc0ca9ef3068a9f5bfa57a27e
CodeArmer <geanmotas@gmail.com>
2025-02-09 03:48:45 -0400

    Added unpic to astro image services

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+import { imageService } from '@unpic/astro/service';
+
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
+  image: {
+    service: imageService(),
+  },
   integrations: [
     preact({
       include: ["src/components/**/*.{jsx,tsx}"]
