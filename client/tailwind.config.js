8e60567eb38d98e600ec37f2e3393856eb623e63
CodeArmer <geanmotas@gmail.com>
2025-02-27 18:28:03 -0400

    Updated index.astro

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+import tailwindcssMotion from "tailwindcss-motion";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        extend: {},
+    },
+    plugins: [tailwindcssMotion],
+};
