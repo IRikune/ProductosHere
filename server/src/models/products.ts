36095cc0b359fbe62aaa8ed85ee217395af94447
CodeArmer <geanmotas@gmail.com>
2025-03-01 05:43:03 -0400

    Updated types and schemas

--- server/src/models/products.ts
+++ server/src/models/products.ts
@@ -1,5 +1,28 @@
 import { kv } from "../../mod.ts"
 import type { Product } from "../types/mod.ts"
+import type { Serie as SerieType } from "../types/mod.ts"
 
-export const getManyProducts = async (): Promise<Product[]> => {
+interface getManyProductsOptions {
+  serie?: SerieType["id"]
+}
+
+interface KvProductsResult {
+  ok: boolean
+  data?: Product | Product[]
+}
+type KvProductsResultMaybe = Promise<KvProductsResult>
+
+// add actual serie by default
+export const getManyProducts = async (
+  { serie = "" }: getManyProductsOptions,
+): KvProductsResultMaybe => {
+  const primaryKey = [serie, "products"]
+  const entries = kv.list<Product>({ prefix: primaryKey })
+  const products: Product[] = []
+  for await (const entry of entries) {
+    const product = entry.value
+    products.push(product)
+  }
+  const res: KvProductsResult = { ok: true, data: products }
+  return res
 }
