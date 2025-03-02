c5828f3e03fb86e92bdc5cc1ddc964382b8975b1
CodeArmer <geanmotas@gmail.com>
2025-03-02 17:10:10 -0400

    Fixed burger menu bug

--- client/src/components/preact/BurgerMenu.tsx
+++ client/src/components/preact/BurgerMenu.tsx
@@ -9,13 +9,13 @@ export function BurgerMenu({ class: className }: { class?: string }) {
             <button
                 type="button"
                 onClick={() => { isOpen.value = !isOpen.value }}
-                class='cursor-pointer w-10 h-10 relative rounded flex justify-center items-center transition duration-300 active:bg-neutral-200 z-30 overflow-hidden starting:opacity-0 *:bg-black *:p-px'>
+                class={`cursor-pointer w-10 h-10 relative rounded flex justify-center items-center transition duration-300 active:bg-neutral-200 z-40 overflow-hidden starting:opacity-0 *:bg-black *:p-px ${className}`}>
                 <div
                     class={`rounded w-[80%] self-start absolute mt-2 ${isOpen.value ? 'animation-top-bar-open' : 'animation-top-bar-close'}`} />
                 <div
                     class={`rounded w-[80%] absolute ${isOpen.value ? 'animation-middle-bar-open' : 'animation-middle-bar-close'}`} />
                 <div
-                    class={`rounded w-[80%] self-end absolute mb-2 ${isOpen.value ? 'animation-bottom-bar-open' : 'animation-bottom-bar-close'} ${className}`}>
+                    class={`rounded w-[80%] self-end absolute mb-2 ${isOpen.value ? 'animation-bottom-bar-open' : 'animation-bottom-bar-close'}`}>
                 </div>
             </button>
             <div>
@@ -28,7 +28,7 @@ function Menu({ isOpen }: { isOpen: Signal<boolean> }) {
     return (
         <div
             class={`
-                fixed bg-[#f4f2ed] left-0 w-dvw h-dvh -top-1 py-10 z-20 flex items-center justify-center flex-col starting:opacity-0 transition-all duration-300 ease-in-out transition-discrete
+                fixed bg-[#f4f2ed] left-0 w-dvw h-dvh -top-1 py-10 z-30 flex items-center justify-center flex-col starting:opacity-0 transition-all duration-300 ease-in-out transition-discrete
                 ${isOpen.value ? 'opacity-100 translate-0' : 'opacity-0 hidden'}`}>
             <a href='/feed'>
                 <Button type='outlined' class='text-3xl my-2'>Feed</Button>
